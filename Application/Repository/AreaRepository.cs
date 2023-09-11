72076edd8af75ae4c943de27542f131562e6ef0e
EstebanSansart <julianestebansanabria@gmail.com>
2023-09-11 18:55:27 -0500

    revert: :fire: BadCodeRemoved
    AreaRepository

--- Application/Repository/AreaRepository.cs
+++ Application/Repository/AreaRepository.cs
@@ -6,29 +6,9 @@ using Persistence;
 namespace Application.Repository;
 public class AreaRepository : GenericRepository<Area>, IAreaRepository
 {
-    protected readonly ApiIncidenciasIContext _context;
+    public readonly ApiIncidenciasIContext _context;
     public AreaRepository(ApiIncidenciasIContext context) : base(context)
     {
         _context = context;
-    }
-    public override async Task<(int totalRegistros, IEnumerable<Area> registros)> GetAllAsync(int pageIndex, int pageSize, string search)
-    {
-        var query = _context.Areas as IQueryable<Area>;
-
-        if (string.IsNullOrEmpty(search))
-        {
-            query = query.Where(p => p.Name.ToLower().Contains(search));
-        }
-
-        var totalRegistros = await query.CountAsync();
-        var registros = await query
-                                .Include(u => u.UserAreas)
-                                .Include(v => v.Places)
-                                .Include(w => w.Incidences)
-                                .Skip((pageIndex -1) * pageSize)
-                                .Take(pageSize)
-                                .ToListAsync();
-
-        return(totalRegistros, registros);
     }
 }
